a862e3b7464e2a132aaa15514b1a38eea1787103
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2023-08-12 20:41:22 -0300

    Fix tabs and set basic structure for sidebar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,7 +1,57 @@
-import React from 'react'
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components'
 
 export const SideBar = () => {
-  return (
-    <div>SideBar</div>
-  )
+	return (
+		<StyledMainContainer>
+			<StyledNavLink to='/'>
+				{/* <House /> */} <span>Home</span>
+			</StyledNavLink>
+			<StyledNavLink to='/'>
+				{/* <House /> */} <span>TCs</span>
+			</StyledNavLink>
+			<StyledNavLink to='/'>
+				{/* <House /> */} <span>Gastos</span>
+			</StyledNavLink>
+
+		</StyledMainContainer>
+	)
 }
+
+
+const StyledMainContainer = styled.div`
+  background-color: #252525;
+  color: #f5f5f5;
+  display: flex;
+  flex-direction: column;
+  padding: 0.5rem;
+  min-width: 4.5rem;
+`;
+
+const StyledNavLink = styled(NavLink)`
+  color: var(--main-color);
+  text-decoration: none;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  svg {
+    font-size: 2rem;
+  }
+
+  span {
+    position: relative;
+    bottom: 0.1rem;
+    font-size: 1rem;
+  }
+
+
+  &:hover {
+    color: var(--main-color);
+  }
+  &.active {
+    font-weight: bold;
+    filter: brightness(1.5);
+  }
+`;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,14 +1,14 @@
 import { useAppSelector } from "../hooks/reduxHooks";
 
 export const HomePage = () => {
-  const creditCards = useAppSelector(state => state.creditCardsState)
-  return (
-    <div>
-      <h1>Home Page</h1>
-      <hr />
-      <pre>
-        {JSON.stringify(creditCards, null, 3)}
-      </pre>
-    </div>
-  );
+	const creditCards = useAppSelector(state => state.creditCardsState)
+	return (
+		<div>
+			<h1>Home Page</h1>
+			<hr />
+			<pre>
+				{JSON.stringify(creditCards, null, 3)}
+			</pre>
+		</div>
+	);
 };
