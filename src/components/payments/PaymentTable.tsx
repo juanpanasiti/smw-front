f7977f1d095570950c4268ac22e91f3a3b057550
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2023-09-06 10:04:04 -0300

    add filter on payment tables

--- src/components/payments/PaymentTable.tsx
+++ src/components/payments/PaymentTable.tsx
@@ -1,7 +1,6 @@
-import { useAppDispatch, useAppSelector } from '../../hooks/reduxHooks'
-import { Expense } from '../../types/expenses';
-import { Payment, Status } from '../../types/payments'
-import { Button, ButtonGroup, Dropdown, DropdownButton, Form, Modal, Table } from 'react-bootstrap'
+import { useAppDispatch, useAppSelector } from '../../hooks/reduxHooks';
+import { Payment, Status } from '../../types/payments';
+import { Button, ButtonGroup, Dropdown, DropdownButton, Form, Modal, Table } from 'react-bootstrap';
 import { StatusIcon } from '../shared/StatusIcon';
 import { faDollarSign, faFileCirclePlus } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -12,105 +11,135 @@ import { defaultPayment } from '../../helpers/defaultValues';
 import { addMessage } from '../../store/slices/messages/messageSlice';
 import { CreditCard } from '../../types/creditCard';
 import { PaymentSummary } from './PaymentSummary';
+import { findExpense } from '../../helpers/expensesHelpers';
 interface Props {
-    payments: Payment[],
+    payments: Payment[];
 }
 export const PaymentTable = ({ payments }: Props) => {
     const { expenses } = useAppSelector(({ expensesState }) => expensesState);
     const { creditCards } = useAppSelector(({ creditCardsState }) => creditCardsState);
-    const dispatch = useAppDispatch()
-    const [showModal, setShowModal] = useState(false)
-    const [selectedPayment, setSelectedPayment] = useState<Payment>({ ...defaultPayment })
-    const [filteredPayments, setFilteredPayments] = useState<Payment[]>([])
-    const [selectedCreditCard, setSelectedCreditCard] = useState<CreditCard | undefined>(undefined)
+    const dispatch = useAppDispatch();
+    const [showModal, setShowModal] = useState(false);
+    const [selectedPayment, setSelectedPayment] = useState<Payment>({ ...defaultPayment });
+    const [filteredPayments, setFilteredPayments] = useState<Payment[]>([]);
+    const [selectedCreditCard, setSelectedCreditCard] = useState<CreditCard | undefined>(undefined);
+    const [paymentFilter, setPaymentFilter] = useState('');
 
     useEffect(() => {
-        if (selectedCreditCard === undefined) {
-            setFilteredPayments(payments)
-        } else {
-            setFilteredPayments(payments.filter(payment => findExpense(payment)?.creditCardId === selectedCreditCard.id))
+        let paymentsToShow = payments;
+        if (selectedCreditCard !== undefined) {
+            paymentsToShow = paymentsToShow.filter(payment => findExpense(expenses, payment)?.creditCardId === selectedCreditCard.id);
         }
-    }, [payments, selectedCreditCard])
+        if (paymentFilter !== '') {
+            paymentsToShow = paymentsToShow.filter(payment => {
+                const expense = findExpense(expenses, payment);
+                if (expense === undefined) {
+                    return false;
+                }
+                return (
+                    expense.ccName.toLowerCase().includes(paymentFilter.toLowerCase()) ||
+                    expense.title.toLowerCase().includes(paymentFilter.toLowerCase()) ||
+                    payment.amount.toString().includes(paymentFilter.toLowerCase()) ||
+                    payment.status.toLowerCase().includes(paymentFilter.toLowerCase()) ||
+                    payment.amount.toString().includes(paymentFilter.toLowerCase())
+                );
+            });
+        }
 
+        setFilteredPayments(paymentsToShow);
+    }, [payments, selectedCreditCard, paymentFilter, expenses]);
 
-
-    const findExpense = (payment: Payment): Expense | undefined => {
-        return expenses.find(expense => expense.id === payment.expenseId)
-    }
     const getStatusClassName = (status: Status): string => {
         switch (status) {
             case 'unconfirmed':
-                return 'table-warning'
+                return 'table-warning';
             case 'confirmed':
-                return 'table-primary'
+                return 'table-primary';
             case 'paid':
-                return 'table-success'
+                return 'table-success';
             case 'canceled':
-                return 'table-danger'
+                return 'table-danger';
             case 'simulated':
-                return 'table-secondary'
+                return 'table-secondary';
 
             default:
-                return ''
+                return '';
         }
-    }
+    };
     const handleUpdate = async (payment: Payment) => {
         try {
-            const updated = await updatePaymentAPI(payment)
-            dispatch(updatePayment({
-                paymentId: payment.id,
-                payment: updated
-            }))
-            dispatch(addMessage({
-                text: 'saving success',
-                title: 'Success',
-                subtitle: '',
-                type: 'success'
-            }))
+            const updated = await updatePaymentAPI(payment);
+            dispatch(
+                updatePayment({
+                    paymentId: payment.id,
+                    payment: updated,
+                }),
+            );
+            dispatch(
+                addMessage({
+                    text: 'saving success',
+                    title: 'Success',
+                    subtitle: '',
+                    type: 'success',
+                }),
+            );
         } catch (error) {
-            dispatch(addMessage({
-                text: 'Error on saving payment',
-                title: 'Saving Error',
-                subtitle: '',
-                type: 'error',
-            }))
-            console.error(error)
+            dispatch(
+                addMessage({
+                    text: 'Error on saving payment',
+                    title: 'Saving Error',
+                    subtitle: '',
+                    type: 'error',
+                }),
+            );
+            console.error(error);
         }
-    }
+    };
     const handleUpdateAmount = (payment: Payment) => {
-        setSelectedPayment(payment)
-        setShowModal(true)
-    }
+        setSelectedPayment(payment);
+        setShowModal(true);
+    };
     const handleAddSubscription = async (payment: Payment) => {
         try {
             const created = await createPaymentAPI({
                 ...payment,
-                status: 'unconfirmed'
-            })
-            dispatch(updatePayment({
-                paymentId: payment.id,
-                payment: created
-            }))
-            dispatch(addMessage({
-                text: 'saving success',
-                title: 'Success',
-                subtitle: '',
-                type: 'success'
-            }))
+                status: 'unconfirmed',
+            });
+            dispatch(
+                updatePayment({
+                    paymentId: payment.id,
+                    payment: created,
+                }),
+            );
+            dispatch(
+                addMessage({
+                    text: 'saving success',
+                    title: 'Success',
+                    subtitle: '',
+                    type: 'success',
+                }),
+            );
         } catch (error) {
-            dispatch(addMessage({
-                text: 'Error on saving payment',
-                title: 'Saving Error',
-                subtitle: '',
-                type: 'error',
-            }))
-            console.error(error)
+            dispatch(
+                addMessage({
+                    text: 'Error on saving payment',
+                    title: 'Saving Error',
+                    subtitle: '',
+                    type: 'error',
+                }),
+            );
+            console.error(error);
         }
-    }
-    const handleCreditCardSelected = (e: React.ChangeEvent<HTMLSelectElement>) => {
-        const ccId = parseInt(e.target.value)
-        setSelectedCreditCard(creditCards.find(cc => cc.id === ccId)) 
-    }
+    };
+    const handleCreditCardSelected = (name: string, value: string) => {
+        if (name === 'ccSelect') {
+            const ccId = parseInt(value);
+            setSelectedCreditCard(creditCards.find(cc => cc.id === ccId));
+        }
+        if (name === 'filterField') {
+            setPaymentFilter(value);
+        }
+    };
 
     return (
         <>
@@ -130,55 +159,73 @@ export const PaymentTable = ({ payments }: Props) => {
                     </tr>
                 </thead>
                 <tbody>
-                    {
-                        filteredPayments.map(payment => {
-                            const expense = findExpense(payment)
-                            const fee = expense?.type === 'purchase' ? `${payment.number}/${expense?.totalInstallments}` : '---'
-                            const amount = new Intl.NumberFormat('es-AR', { style: 'currency', currency: 'ARS' }).format(payment.amount)
-                            return (
-                                <tr key={payment.id} className={getStatusClassName(payment.status)}>
-                                    <td>{expense?.creditCardName}</td>
-                                    <td>{expense?.title}</td>
-                                    <td><StatusIcon status={payment.status} /></td>
-                                    <td>{fee}</td>
-                                    <td>{amount}</td>
-                                    <td>
-                                        <ButtonGroup aria-label="Actions">
-                                            {
-                                                payment.status === 'simulated' ?
-                                                    <Button variant="primary" onClick={() => handleAddSubscription(payment)}><FontAwesomeIcon icon={faFileCirclePlus} /></Button> :
-                                                    <Button variant="success" onClick={() => handleUpdateAmount(payment)}><FontAwesomeIcon icon={faDollarSign} /></Button>
-                                            }
-                                            <DropdownButton disabled={payment.status === 'simulated'} as={ButtonGroup} title="Status" id="bg-status-dropdown">
-                                                {payment.status !== 'unconfirmed' && <Dropdown.Item onClick={() => handleUpdate({ ...payment, status: 'unconfirmed' })}>Unconfirmed</Dropdown.Item>}
-                                                {payment.status !== 'confirmed' && <Dropdown.Item onClick={() => handleUpdate({ ...payment, status: 'confirmed' })}>Confirmed</Dropdown.Item>}
-                                                {payment.status !== 'paid' && <Dropdown.Item onClick={() => handleUpdate({ ...payment, status: 'paid' })}>Paid</Dropdown.Item>}
-                                            </DropdownButton>
-                                        </ButtonGroup>
-                                    </td>
-                                </tr>
-                            )
-                        })
-                    }
+                    {filteredPayments.map(payment => {
+                        const expense = findExpense(expenses, payment);
+                        const fee = expense?.type === 'purchase' ? `${payment.number}/${expense?.totalInstallments}` : '---';
+                        const amount = new Intl.NumberFormat('es-AR', { style: 'currency', currency: 'ARS' }).format(payment.amount);
+                        return (
+                            <tr key={payment.id} className={getStatusClassName(payment.status)}>
+                                <td>{expense?.creditCardName}</td>
+                                <td>{expense?.title}</td>
+                                <td>
+                                    <StatusIcon status={payment.status} />
+                                </td>
+                                <td>{fee}</td>
+                                <td>{amount}</td>
+                                <td>
+                                    <ButtonGroup aria-label="Actions">
+                                        {payment.status === 'simulated' ? (
+                                            <Button variant="primary" onClick={() => handleAddSubscription(payment)}>
+                                                <FontAwesomeIcon icon={faFileCirclePlus} />
+                                            </Button>
+                                        ) : (
+                                            <Button variant="success" onClick={() => handleUpdateAmount(payment)}>
+                                                <FontAwesomeIcon icon={faDollarSign} />
+                                            </Button>
+                                        )}
+                                        <DropdownButton
+                                            disabled={payment.status === 'simulated'}
+                                            as={ButtonGroup}
+                                            title="Status"
+                                            id="bg-status-dropdown">
+                                            {payment.status !== 'unconfirmed' && (
+                                                <Dropdown.Item onClick={() => handleUpdate({ ...payment, status: 'unconfirmed' })}>
+                                                    Unconfirmed
+                                                </Dropdown.Item>
+                                            )}
+                                            {payment.status !== 'confirmed' && (
+                                                <Dropdown.Item onClick={() => handleUpdate({ ...payment, status: 'confirmed' })}>
+                                                    Confirmed
+                                                </Dropdown.Item>
+                                            )}
+                                            {payment.status !== 'paid' && (
+                                                <Dropdown.Item onClick={() => handleUpdate({ ...payment, status: 'paid' })}>Paid</Dropdown.Item>
+                                            )}
+                                        </DropdownButton>
+                                    </ButtonGroup>
+                                </td>
+                            </tr>
+                        );
+                    })}
                 </tbody>
                 <AmountUpdateModal show={showModal} handleClose={() => setShowModal(false)} payment={selectedPayment} updateAmount={handleUpdate} />
             </Table>
         </>
-    )
-}
+    );
+};
 
 interface ModalProps {
-    show: boolean,
-    handleClose: () => void,
-    updateAmount: (payment: Payment) => void,
-    payment: Payment,
+    show: boolean;
+    handleClose: () => void;
+    updateAmount: (payment: Payment) => void;
+    payment: Payment;
 }
 const AmountUpdateModal = ({ show, handleClose, payment, updateAmount }: ModalProps) => {
-    const [amount, setAmount] = useState<number>(payment.amount)
+    const [amount, setAmount] = useState<number>(payment.amount);
     const handleSave = () => {
-        updateAmount({ ...payment, amount })
-        handleClose()
-    }
+        updateAmount({ ...payment, amount });
+        handleClose();
+    };
     return (
         <Modal show={show} onHide={handleClose}>
             <Modal.Header closeButton>
@@ -193,7 +240,7 @@ const AmountUpdateModal = ({ show, handleClose, payment, updateAmount }: ModalPr
                             step="0.01"
                             defaultValue={payment.amount}
                             autoFocus
-                            onChange={(e) => setAmount(parseFloat(e.target.value))}
+                            onChange={e => setAmount(parseFloat(e.target.value))}
                         />
                     </Form.Group>
                 </Form>
@@ -207,5 +254,5 @@ const AmountUpdateModal = ({ show, handleClose, payment, updateAmount }: ModalPr
                 </Button>
             </Modal.Footer>
         </Modal>
-    )
-}
+    );
+};
